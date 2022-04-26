ea79e3f08f073afed5d464624314f59f7f1cc7c0
gzapatas <GZ_junior@hotmail.com>
2022-04-26 15:53:09 -0500

    prueba nueva rama

--- main.go
+++ main.go
@@ -12,10 +12,15 @@ func Suma(a, b int) int {
 	return a + b
 }
 
+func Resta(a, b int) int {
+	return a - b
+}
+
 func main() {
 	fmt.Printf("Version = %v\n", Version)
 	fmt.Printf("Time = %v\n", build.Time)
 	fmt.Printf("User = %v\n", build.User)
 
 	fmt.Printf("Suma = %v\n", Suma(2, 5))
+	fmt.Printf("Resta = %v\n", Resta(2, 5))
 }
