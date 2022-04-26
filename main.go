0887cd49ef7bdbf2507273d85fdb0f447dba36cc
gzapatas <GZ_junior@hotmail.com>
2022-04-26 15:48:40 -0500

    test

--- main.go
+++ main.go
@@ -8,8 +8,14 @@ import (
 
 var Version string = "deployment"
 
+func Suma(a, b int) int {
+	return a + b
+}
+
 func main() {
 	fmt.Printf("Version = %v\n", Version)
 	fmt.Printf("Time = %v\n", build.Time)
 	fmt.Printf("User = %v\n", build.User)
+
+	fmt.Printf("Suma = %v\n", Suma(2, 5))
 }
