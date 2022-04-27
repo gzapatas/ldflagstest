fb33bb02381c7e6ad4edaaacfce44518f56dd2ad
gzapatas <GZ_junior@hotmail.com>
2022-04-26 22:30:09 -0500

    Prueba produccion

--- main.go
+++ main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
 
 	"github.com/gzapatas/ldflagstest/build"
@@ -20,7 +21,69 @@ func Multiplicacion(a, b int) int {
 	return a * b
 }
 
+type EventMeter struct {
+	accounting  interface{}
+	eventTypeId uint8
+}
+
+type Meters struct {
+	Turnover  int64 `json:"turnover"`
+	Totalwins int64 `json:"totalwins"`
+}
+
 func main() {
+	var smibs []string = []string{
+		"Smib1",
+	}
+
+	//253 -> RESET , 254 -> INITIAL, 255 -> FINAL, 252 -> ROLLOVER
+
+	for range smibs {
+		//Llamar al endpoint de events
+		var events []EventMeter = []EventMeter{
+			{
+				accounting: map[string]interface{}{
+					"turnover":  5000,
+					"totalwins": 6000,
+				},
+				eventTypeId: uint8(254),
+			},
+			{
+				accounting: map[string]interface{}{
+					"turnover":  8000,
+					"totalwins": 10000,
+				},
+				eventTypeId: uint8(255),
+			},
+		}
+
+		var deltaTurnover int64 = 0
+		var deltaTotalwins int64 = 0
+
+		for _, event := range events {
+			var meter Meters
+			data, _ := json.Marshal(event.accounting)
+
+			json.Unmarshal(data, &meter)
+
+			if event.eventTypeId == 254 {
+				deltaTurnover -= meter.Turnover
+				deltaTotalwins -= meter.Totalwins
+			}
+
+			if event.eventTypeId == 255 {
+				deltaTurnover += meter.Turnover
+				deltaTotalwins += meter.Totalwins
+			}
+		}
+
+		var production = deltaTotalwins - deltaTurnover
+		//ACA DEBES GUARDAR EN BASE DE DATOS
+		fmt.Printf("deltaTotalwins %v\n", deltaTotalwins)
+		fmt.Printf("deltaTurnover %v\n", deltaTurnover)
+		fmt.Printf("PRODUCCION %v\n", production)
+	}
+
 	fmt.Printf("Version = %v\n", Version)
 	fmt.Printf("Time = %v\n", build.Time)
 	fmt.Printf("User = %v\n", build.User)
